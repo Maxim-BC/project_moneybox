17e32d9af9c5de2123ef7410332eca35166b0e24
caimon37rus <caimon37rus@gmail.com>
2021-07-05 20:47:17 +0300

    !

--- src/components/Assistant.js
+++ src/components/Assistant.js
@@ -3,17 +3,13 @@ import "./Assistant.css";
 import Panda from "./image/Panda.png";
 import "./TargetList";
 export default class Assistant extends Component {
-
-
   render() {
     return (
-      
       <div>
-       
         <div className="oblako">
           <p className="oblako-text">
             Привет! я подскажу тебе как пользоваться этим приложением. Для
-            создания новой цели кликни кнопку
+            создания новой цели кликни кнопку!
           </p>
 
           <div
