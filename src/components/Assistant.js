b1287d9bd5f063ff11dbc4f8838f7f52a84fe70b
caimon37rus <caimon37rus@gmail.com>
2021-07-08 21:11:12 +0300

    ассистент - реакция на цели

--- src/components/Assistant.js
+++ src/components/Assistant.js
@@ -3,24 +3,43 @@ import "./Assistant.css";
 import Panda from "./image/Panda.png";
 import "./TargetList";
 export default class Assistant extends Component {
-  render() {
-    return (
-      <div>
-        <div className="oblako">
-          <p className="oblako-text">
-            Привет! я подскажу тебе как пользоваться этим приложением. Для
-            создания новой цели кликни кнопку!
-          </p>
+  num() {
+    if (this.props.targetCount > 0) {
+      return (
+        <div>
+          <div className="oblako">
+            <p className="oblako-text">
+              Привет! я подскажу тебе как пользоваться этим приложением. Для
+              создания новой цели кликни кнопку!
+            </p>
 
-          <div
-            onClick={() => this.props.funcChangeRight("NewTargetForm")}
-            className="plus-assistant"
-          >
-            +
+            <div
+              onClick={() => this.props.funcChangeRight("NewTargetForm")}
+              className="plus-assistant"
+            >
+              +
+            </div>
+          </div>
+          <img className="assistant-panda" src={Panda} alt="panda" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <div className="oblako">
+            <p className="oblako-text">
+              Привет! У тебя {this.props.targetCount} целей , выбери нужную!
+            </p>
+          </div>
+          <div>
+            <img className="assistant-panda" src={Panda} alt="panda" />
           </div>
         </div>
-        <img className="assistant-panda" src={Panda} alt="panda" />
-      </div>
-    );
+      );
+    }
+  }
+
+  render() {
+    return <div>{this.num()}</div>;
   }
 }
--- src/components/Workplace.js
+++ src/components/Workplace.js
@@ -7,161 +7,158 @@ import TargetInfo from "./TargetInfo";
 import PaymentSchedule from "./PaymentSchedule";
 
 export default class Workplace extends Component {
-    state = {
-        rightActiveComponent: "Assistant",
-        activeTarget: "",
-        targets: [
-            {
-                id: 1,
-                targetName: "Автомобиль",
-                sum: 3000000,
-                percent: 0.01,
-                period: 18,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-            {
-                id: 2,
-                targetName: "Дача",
-                sum: 100000,
-                percent: 0.01,
-                period: 12,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-            {
-                id: 3,
-                targetName: "Вилла",
-                sum: 100000,
-                percent: 0.01,
-                period: 12,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-            {
-                id: 4,
-                targetName: "Полет в космос",
-                sum: 100000,
-                percent: 0.01,
-                period: 12,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-            {
-                id: 5,
-                targetName: "Купить Google",
-                sum: 100000,
-                percent: 0.01,
-                period: 12,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-            {
-                id: 6,
-                targetName: "Смартфон",
-                sum: 100000,
-                percent: 0.01,
-                period: 12,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-            {
-                id: 7,
-                targetName: "Смартфон",
-                sum: 100000,
-                percent: 0.01,
-                period: 12,
-                payment: 8333.33,
-                profit: 1231,
-                startDate: "2021-06-30",
-            },
-        ],
-    };
+  state = {
+    rightActiveComponent: "Assistant",
+    activeTarget: "",
+    targets: [
+      {
+        id: 1,
+        targetName: "Автомобиль",
+        sum: 3000000,
+        percent: 0.01,
+        period: 18,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+      {
+        id: 2,
+        targetName: "Дача",
+        sum: 100000,
+        percent: 0.01,
+        period: 12,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+      {
+        id: 3,
+        targetName: "Вилла",
+        sum: 100000,
+        percent: 0.01,
+        period: 12,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+      {
+        id: 4,
+        targetName: "Полет в космос",
+        sum: 100000,
+        percent: 0.01,
+        period: 12,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+      {
+        id: 5,
+        targetName: "Купить Google",
+        sum: 100000,
+        percent: 0.01,
+        period: 12,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+      {
+        id: 6,
+        targetName: "Смартфон",
+        sum: 100000,
+        percent: 0.01,
+        period: 12,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+      {
+        id: 7,
+        targetName: "Смартфон",
+        sum: 100000,
+        percent: 0.01,
+        period: 12,
+        payment: 8333.33,
+        profit: 1231,
+        startDate: "2021-06-30",
+      },
+    ],
+  };
 
-    changeRight = (newActiveComponent) => {
-        this.setState({
-            rightActiveComponent: newActiveComponent,
-        });
-    };
+  changeRight = (newActiveComponent) => {
+    this.setState({
+      rightActiveComponent: newActiveComponent,
+    });
+  };
 
-    changeActiveTarget = (newActiveTarget) => {
-        this.setState({
-            activeTarget: newActiveTarget,
-        });
-    };
+  changeActiveTarget = (newActiveTarget) => {
+    this.setState({
+      activeTarget: newActiveTarget,
+    });
+  };
 
-    addNewTarget = (newTarget) => {
-        this.setState({
-            targets: newTarget,
-        });
-    };
+  addNewTarget = (newTarget) => {
+    this.setState({
+      targets: newTarget,
+    });
+  };
 
-    handleChangeRight = (event) => {
-        event.preventDefault();
-        this.setState({
-            rightActiveComponent: event.target.value,
-        });
-    };
+  handleChangeRight = (event) => {
+    event.preventDefault();
+    this.setState({
+      rightActiveComponent: event.target.value,
+    });
+  };
 
-    render() {
-        return (
-            <>
-                <div>
-                    <select onChange={this.handleChangeRight}>
-                        <option value="Assistant">Assistant</option>
-                        <option value="NewTargetForm">NewTargetForm</option>
-                        <option value="TargetInfo">TargetInfo</option>
-                        <option value="PaymentSchedule">PaymentSchedule</option>
-                    </select>
-                </div>
-                <br />
-                <div className="main-container">
-                    <div className="left-zone">
-                        <div className="scroll-bar">
-                            <TargetPlace
-                                funcChangeRight={this.changeRight}
-                                funcChangeActiveTarget={this.changeActiveTarget}
-                                targets={this.state.targets}
-                            />
-                        </div>
-                    </div>
-                    <div className="slider">||</div>
-                    <div className="right-zone">
-                        <div className="scroll-bar">
-                            {this.state.rightActiveComponent ===
-                                "Assistant" && (
-                                <Assistant funcChangeRight={this.changeRight} />
-                            )}
-                            {this.state.rightActiveComponent ===
-                                "NewTargetForm" && (
-                                <NewTargetForm
-                                    funcChangeRight={this.changeRight}
-                                    funcDddNewTarget={this.addNewTarget}
-                                />
-                            )}
-                            {this.state.rightActiveComponent ===
-                                "TargetInfo" && (
-                                <TargetInfo
-                                    funcChangeRight={this.changeRight}
-                                    activeTarget={this.state.activeTarget}
-                                />
-                            )}
-                            {this.state.rightActiveComponent ===
-                                "PaymentSchedule" && (
-                                <PaymentSchedule
-                                    funcChangeRight={this.changeRight}
-                                />
-                            )}
-                        </div>
-                    </div>
-                </div>
-            </>
-        );
-    }
+  render() {
+    return (
+      <>
+        <div>
+          <select onChange={this.handleChangeRight}>
+            <option value="Assistant">Assistant</option>
+            <option value="NewTargetForm">NewTargetForm</option>
+            <option value="TargetInfo">TargetInfo</option>
+            <option value="PaymentSchedule">PaymentSchedule</option>
+          </select>
+        </div>
+        <br />
+        <div className="main-container">
+          <div className="left-zone">
+            <div className="scroll-bar">
+              <TargetPlace
+                funcChangeRight={this.changeRight}
+                funcChangeActiveTarget={this.changeActiveTarget}
+                targets={this.state.targets}
+              />
+            </div>
+          </div>
+          <div className="slider">||</div>
+          <div className="right-zone">
+            <div className="scroll-bar">
+              {this.state.rightActiveComponent === "Assistant" && (
+                <Assistant
+                  funcChangeRight={this.changeRight}
+                  targetCount={this.state.targets.length}
+                />
+              )}
+              {this.state.rightActiveComponent === "NewTargetForm" && (
+                <NewTargetForm
+                  funcChangeRight={this.changeRight}
+                  funcDddNewTarget={this.addNewTarget}
+                />
+              )}
+              {this.state.rightActiveComponent === "TargetInfo" && (
+                <TargetInfo
+                  funcChangeRight={this.changeRight}
+                  activeTarget={this.state.activeTarget}
+                />
+              )}
+              {this.state.rightActiveComponent === "PaymentSchedule" && (
+                <PaymentSchedule funcChangeRight={this.changeRight} />
+              )}
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
 }
