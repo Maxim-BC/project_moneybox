4a693e56536007a9fedcb48549157dc08df41f36
Makogon Andrey <76115437+makogonao@users.noreply.github.com>
2021-07-10 00:45:47 +0500

    теперь таргеты сохраняются в браузере. УРА.

--- src/components/TargetInfo.js
+++ src/components/TargetInfo.js
@@ -10,7 +10,10 @@ export default class TargetInfo extends Component {
                     <h2>{this.props.activeTarget.targetName}</h2>
                 </div>
                 <div className="general-info">
-                    <p>Дата открытия вклада: {this.props.activeTarget.startDate}</p>
+                    <p>
+                        Дата открытия вклада:{" "}
+                        {this.props.activeTarget.startDate}
+                    </p>
                     <p>Дата закрытия вклада: {/* Вычислить  */}</p>
                     <p>Процентная ставка: {this.props.activeTarget.percent}</p>
                     <p>Срок вклада (мес): {this.props.activeTarget.period}</p>
@@ -23,7 +26,15 @@ export default class TargetInfo extends Component {
                         <button className="orange-btn">График платежей</button>
                     </div> */}
                     <div>
-                        <button className="red-btn" onClick={()=>this.props.funcDelTargetByID(this.props.activeTarget.id)}>Удалить цель</button>
+                        <button
+                            className="red-btn"
+                            onClick={() => {
+                                this.props.funcDelTargetByID(this.props.activeTarget.id);
+                                this.props.funcChangeRight('Assistant')
+                            }}
+                        >
+                            Удалить цель
+                        </button>
                     </div>
                 </div>
             </div>
